213024d0557df95813a184e0a042e2c8601e84dc
kimjames791 <kimjames791@gmail.com>
2023-10-07 11:12:42 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	char c;
-	
-	for (c = 'a', i < 'z';c++
-	putchar(c)
-	putchar("\n);
 
-	return (0)
-	
+	for (c = 'a'; c <= 'z' ; c++)
+	putchar(c);
+	for (c = 'A'; c <= 'Z' ; c++)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
 }
