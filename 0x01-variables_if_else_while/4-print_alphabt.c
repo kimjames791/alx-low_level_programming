207091efed63b388795560e3fe48688849ad727f
kimjames791 <kimjames791@gmail.com>
2023-10-07 21:59:52 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,11 @@ int main(void)
 	c = 'a';
 	while (c <= 'z')
 	{
-	if ((c != 'e' && c != 'q') && c <= 'z')
+	if (c != 'e' && c != 'q')
 	putchar(c);
-	putchar('\n');
 	c++;
 	}
+	putchar('\n');
 	return (0);
 }
 
