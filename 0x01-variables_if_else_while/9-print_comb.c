cd3aa93ebe86e20aa43a3e94d06ddb697f9612f7
kimjames791 <kimjames791@gmail.com>
2023-10-08 08:41:08 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main  - this is the main entry point
+ * Return: return 0 on success
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+	putchar(k + '0');
+	}
+	if (k < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
