cb65402186cea4ed71cdf6f7fce2211f3b9d0756
kimjames791 <kimjames791@gmail.com>
2023-10-08 06:34:45 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - this is the main entry point
+ *  Return: return 0 always on success
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'z';
+	while (d <= 'a')
+	{
+	putchar(d);
+	d++;
+	}
+	putchar('\n');
+	return (0);
+}
+
