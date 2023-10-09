00d503ba19323def71fa3257970334d064c7220d
kimjames791 <kimjames791@gmail.com>
2023-10-08 20:03:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints a line with puts function
+ * Return: return 0 always on success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
