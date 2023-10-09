f93fce79d0aa8e3f339202c81c13408fd653cbfa
kimjames791 <kimjames791@gmail.com>
2023-10-08 21:26:47 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - proper grammer art outcome
+ * Return 0 always on success
+ */
+
+int main(void)
+{
+	printtf("with proper grammar, but the outcome is a piece of art,\n")
+
+	return (0);
+}
