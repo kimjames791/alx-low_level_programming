77291151ff16cc0200221cacc0ce9155e86a9d39
kimjames791 <kimjames791@gmail.com>
2023-10-09 03:38:43 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,11 @@
  */
 int main(void)
 {
-
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float f;
-
 printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of int : %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
