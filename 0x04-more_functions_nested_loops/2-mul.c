f592e131eb90d6b03868666c4d069f4991edd71a
kimjames791 <kimjames791@gmail.com>
2023-10-13 14:44:35 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul.c -finction that checks for a digit from o
+ * @a: a is avariable
+ * @b: b is a variable
+ * Return: always 0.
+ */
+
+int mul(int a, int b)
+
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
