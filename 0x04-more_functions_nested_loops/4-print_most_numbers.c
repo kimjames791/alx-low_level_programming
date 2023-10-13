9b3843dc405bf57e6ab4519327834eaf2fbc6a3b
kimjames791 <kimjames791@gmail.com>
2023-10-13 16:10:57 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - function thst checks for a digit from 0 to 9
+ * Return: 0 always on success
+ */
+
+void print_most_numbers(void)
+{
+	int d;
+
+	for (d = 48; c < 58; c++)
+	{
+	if (d != 50)
+	{
+	if (d != 52)
+	{
+	_putchar(d);
+	}
+	}
+	}
+	_putchar('\n')
+}
