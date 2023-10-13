68b8180c8d0b7b3b5f0aec9e28f1c45c5a9587ce
kimjames791 <kimjames791@gmail.com>
2023-10-13 14:18:46 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper -progi check for upper case
+ * @c: variable text
+ * Return: always 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
