b5260ee998eabda7e5ab73654762a30436b18af9
kimjames791 <kimjames791@gmail.com>
2023-10-17 04:37:54 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,14 +5,4 @@
 #include <unistd.h>
 
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
-void print_array(int *a, int n)
-
 #endif
