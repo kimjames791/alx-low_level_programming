2278462ab7f67915818c1b0462985f05d38c3aac
kimjames791 <kimjames791@gmail.com>
2023-10-19 16:24:03 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int destlen = 0;
-	int srclen = 0;
-	int i;
+	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		destlen++;
-	for (i = 0; src[i] != '\0'; i++)
-		srclen++;
-	for (i = 0; i <= srclen; i++)
+	i = 0;
+
+	while (dest[i] != '\0')
 	{
-		dest[destlen + i] = src[i];
+		i++;
 	}
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
