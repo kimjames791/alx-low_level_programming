95db66a65a5dad130c0fb316505460ed6f6a12ff
kimjames791 <kimjames791@gmail.com>
2023-10-31 14:06:57 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,23 +6,19 @@
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int H, M;
 
-	for (a = 0; a <= 2; a++)
+	for (H = 0; H < 24; H++)
 	{
-	for (b = 0; b <= 9; b++)
-	}				
-	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-	{
-	for (c = 0; c <= 5; c++)
-	}
-	for (d = 0; d <= 9; d++)
-	{
-	_putchar(a + '0');
-	_putchar(b + '0');
-	_putchar(58);
-	_putchar(c + '0');
-	_putchar(d + '0');
-	_putchar('\n');
-	}
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':')
+			_putchar((M / 10) + '0');
+			_putchar((M / 10) '0');
+			_putchar('\n');
+
+		}
+
 }
