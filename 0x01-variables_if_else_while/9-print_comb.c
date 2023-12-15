4864495ad2021c0743297c5a28a14ad5c59f944b
kimjames791 <kimjames791@gmail.com>
2023-12-15 10:38:19 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,7 @@ int main(void)
 	int k;
 
 	for (k = 0; k < 10; k++)
-	{
-	putchar(k + '0');
-	}
+	{putchar(k + '0'};
 	if (k < 9)
 	{
 	putchar(',');
@@ -19,4 +17,5 @@ int main(void)
 	putchar('\n');
 	}
 	return (0);
+	}
 }
