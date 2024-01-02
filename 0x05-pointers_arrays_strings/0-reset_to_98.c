a9470a20e1b2b726627c600c3242b91491a16b7e
kimjames791 <kimjames791@gmail.com>
2024-01-02 12:48:55 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 -a function that takes a pointer an int a
  * and updates the value the value pointed to 98
- * @n: input
+ * @n:- input
  * Return: n
  */
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,2 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
-
-void puts2(char *str)
-#endif
+int _putchar(char c);
+void reset_to_98(int *n);
