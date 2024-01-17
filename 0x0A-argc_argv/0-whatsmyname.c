6bfadfad6748ba0df11188ff086e275e1450a2df
kimjames791 <kimjames791@gmail.com>
2024-01-17 09:50:09 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints program name fillowed by anew line
+ * @argc: number of arguments passed
+ * @argv: array of arguments
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
