6ce34171a11ce988810cedc95a9274c9500453aa
kimjames791 <kimjames791@gmail.com>
2024-02-01 10:38:32 -0800

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - free  list_t list
+ * @head: head of the linked lists
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *nxt;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		nxt = current->next;
+		free(current->str);
+		free(current);
+		current = nxt;
+	}
+}
