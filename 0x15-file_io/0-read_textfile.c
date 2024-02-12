1c9df5430503b3061461717fba018b9e8cb1cba8
kimjames791 <kimjames791@gmail.com>
2024-02-12 10:51:20 -0800

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+* read_textfile - checks the code for Holberton School learners
+* @filename: file to be  read writen
+* @letters: number of letters to be read and writen
+* Return: letters printed if success
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t nletters;
+	int file;
+	char *text;
+
+	if (!filename)
+		return (0);
+	text = malloc(sizeof(char) * letters + 1);
+	if (text == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+	nletters = read(file, text, sizeof(char) * letters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	nletters = write(STDOUT_FILENO, text, nletters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	free(text);
+	close(file);
+	return (nletters);
+}
